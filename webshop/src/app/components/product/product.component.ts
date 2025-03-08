2d4047ac3ebedefe24e55afe578d354ba9ccc31f
ThomasStr91 <thomas.struski@sviss.at>
2025-03-08 19:37:08 +0100

    Add Card View of all products

--- webshop/src/app/components/product/product.component.ts
+++ webshop/src/app/components/product/product.component.ts
@@ -1,9 +1,14 @@
 import { Component, Input } from '@angular/core';
 import { Goods } from '../../interfaces/goods';
+import { MatButton } from '@angular/material/button';
+import { MatCard, MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-product',
-  imports: [],
+  imports: [ 
+    MatButton,
+    MatCardModule
+  ],
   templateUrl: './product.component.html',
   styleUrl: './product.component.scss'
 })
